248d097713dc894bfb73a3d7a1f449a85997a777
hunter.mayne <hunter.mayne@zerolatencyvr.com>
2023-12-22 16:33:41 +1100

    Change to hawker Hunter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import hawkerHunterImage from './Hunter01.jpg';
+
+const specifications = [
+    { spec: 'Top Speed', value: 'Mach 0.94 (735 mph, 1,185 km/h at 36,000 ft)' },
+    { spec: 'Takeoff Weight', value: '24,600 lb (11,158 kg)' },
+    // Add more specifications here
+    // ...
+    { spec: 'Specification 10', value: 'Value 10' },
+];
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+    return (
+        <div>
+            <h1>Hawker Hunter Specs</h1>
+            <img src={hawkerHunterImage} alt="Hawker Hunter" />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Specification</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {specifications.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.spec}</td>
+                            <td>{item.value}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
